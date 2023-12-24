2e61adf65fe04bad8a7ae5d2db5939893a46b7cf
statiolake <statiolake@gmail.com>
2023-12-24 12:18:55 +0900

    fix: remove dependencies on my personal dotfiles

--- .vim/vimrc.lua
+++ .vim/vimrc.lua
@@ -1,14 +1,13 @@
-local cmd = require("rc.lib.command")
-local k = require("rc.lib.keybind")
-local vimfn = require("rc.lib.vimfn")
-local env = require("rc.lib.env")
+local is_unix = vim.fn.has("unix") == 1
+local is_win = vim.fn.has("win32") == 1
+local is_wsl = is_unix and vim.fn.stridx(vim.fn.system("uname -a"), "WSL2") >= 0
 
 local clipboard
-if env.is_unix and b(vim.fn.executable("wl-copy")) then
+if is_unix and vim.fn.executable("wl-copy") ~= 0 then
 	clipboard = "wl-copy"
-elseif env.is_unix and b(vim.fn.executable("xsel")) then
+elseif is_unix and vim.fn.executable("xsel") ~= 0 then
 	clipboard = "xsel -bo"
-elseif env.is_win or env.is_wsl then
+elseif is_win or is_wsl then
 	clipboard = "clip.exe"
 end
 
@@ -18,7 +17,7 @@ local function open_result(dir, id)
 		return
 	end
 	local bufname = vim.fn.expand("%:t")
-	if not string.ends_with(bufname, ".txt.stderr") then
+	if not vim.endswith(bufname, ".txt.stderr") then
 		vim.cmd("tabnew")
 	end
 	id = string.format("%04d", id)
@@ -28,37 +27,37 @@ end
 local function parse_bufname()
 	local dir = vim.fn.expand("%:h")
 	local bufname = vim.fn.expand("%:t")
-	if not string.ends_with(bufname, ".txt.stderr") then
+	if not vim.endswith(bufname, ".txt.stderr") then
 		return nil
 	end
 	return dir, tonumber(string.sub(bufname, 1, 4))
 end
 
-cmd.add("Bundle", string.format("!cargo xtask bundle | %s", clipboard))
-cmd.add("Run", "!python testing/run_tests.py")
-cmd.add("Visualize", function(ctx)
+vim.api.nvim_create_user_command("Bundle", string.format("!cargo xtask bundle | %s", clipboard), {})
+vim.api.nvim_create_user_command("Run", "!python testing/run_tests.py", {})
+vim.api.nvim_create_user_command("Visualize", function(ctx)
 	local id = string.format("%04d", tonumber(ctx.args[1]))
-	local in_file_name = vimfn.expand(string.format("testing/in/%s.txt", id))
-	local out_file_name = vimfn.expand(string.format("testing/out/%s.txt", id))
+	local in_file_name = vim.fn.expand(string.format("testing/in/%s.txt", id))
+	local out_file_name = vim.fn.expand(string.format("testing/out/%s.txt", id))
 	vim.cmd(string.format("!%s < %s", clipboard, in_file_name))
 	vim.cmd("sleep 1")
 	vim.cmd(string.format("!%s < %s", clipboard, out_file_name))
 end, { nargs = 1 })
-cmd.add("Result", function(ctx)
+vim.api.nvim_create_user_command("Result", function(ctx)
 	open_result(nil, tonumber(ctx.args[1]))
 end, { nargs = 1 })
-cmd.add("ResultNext", function()
+vim.api.nvim_create_user_command("ResultNext", function()
 	local dir, id = parse_bufname()
 	if id then
 		open_result(dir, id + 1)
 	end
-end)
-cmd.add("ResultPrev", function()
+end, {})
+vim.api.nvim_create_user_command("ResultPrev", function()
 	local dir, id = parse_bufname()
 	if id then
 		open_result(dir, id - 1)
 	end
-end)
+end, {})
 
-k.nno("<C-S-N>", k.cmd("ResultNext"))
-k.nno("<C-S-P>", k.cmd("ResultPrev"))
+vim.keymap.set({ "n" }, "<C-S-N>", "<cmd>ResultNext<CR>")
+vim.keymap.set({ "n" }, "<C-S-P>", "<cmd>ResultPrev<CR>")
