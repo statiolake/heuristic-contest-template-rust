71224d36dc361e24d8c031eea6b7a230a4879408
statiolake <statiolake@gmail.com>
2024-06-01 11:02:06 +0900

    feat: move Solution trait to solutions crate
    We need to include io crate from types crate to define Solution trait. But
    this makes it impossible to import types crate in io crate.

--- solutions/src/lib.rs
+++ solutions/src/lib.rs
@@ -1,12 +1,25 @@
 extern crate io;
 extern crate types;
 
-use io::InitInput;
-use types::Solution;
+use std::ops::ControlFlow;
+
+use io::{InitInput, Output, TurnInput};
 
 pub mod naive;
 
 use naive::NaiveSolution;
+
+pub trait Solution {
+    fn name() -> &'static str
+    where
+        Self: Sized;
+
+    fn init(input: InitInput) -> Self
+    where
+        Self: Sized;
+
+    fn think(&mut self, turn: TurnInput) -> ControlFlow<Output, Output>;
+}
 
 macro_rules! define_solutions {
     ($($solution:ident),*$(,)?) => {
--- types/src/lib.rs
+++ types/src/lib.rs
@@ -1,17 +1,1 @@
-extern crate io;
 
-use std::ops::ControlFlow;
-
-use io::{InitInput, Output, TurnInput};
-
-pub trait Solution {
-    fn name() -> &'static str
-    where
-        Self: Sized;
-
-    fn init(input: InitInput) -> Self
-    where
-        Self: Sized;
-
-    fn think(&mut self, turn: TurnInput) -> ControlFlow<Output, Output>;
-}
