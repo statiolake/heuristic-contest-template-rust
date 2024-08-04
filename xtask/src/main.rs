8682a67cdeee27793c9b409d1c24043a3cd3a141
statiolake <statiolake@gmail.com>
2024-08-04 11:24:54 +0900

    feat: ensure to execute xtasks in project root

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,5 +1,8 @@
 use anyhow::{bail, ensure, Result};
-use std::env::args;
+use std::{
+    env::{self, args},
+    path::PathBuf,
+};
 
 pub mod bundle;
 pub mod test;
@@ -7,10 +10,33 @@ pub mod test;
 fn main() -> Result<()> {
     let args: Vec<String> = args().collect();
     ensure!(args.len() > 1, "no task specified");
+    ensure_project_root()?;
 
     match &*args[1] {
         "bundle" => bundle::main(&args[2..]),
         "test" => test::main(&args[2..]),
         _ => bail!("unknown task: {}", args[1]),
+    }
+}
+
+fn ensure_project_root() -> Result<()> {
+    let manifest_path = find_upwards("Cargo.toml")?;
+    let project_root = manifest_path.parent().expect("Cargo.toml has no parent");
+    env::set_current_dir(project_root)?;
+
+    Ok(())
+}
+
+fn find_upwards(name: &str) -> Result<PathBuf> {
+    let mut current_dir = env::current_dir()?;
+    loop {
+        let path = current_dir.join(name);
+        if path.is_file() {
+            return Ok(path);
+        }
+
+        if !current_dir.pop() {
+            bail!("could not find {}", name);
+        }
     }
 }
